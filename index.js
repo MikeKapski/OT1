9f8e0f141ae8711358a9e56f5e6f486fe10471ed
unknown <webbsu@mail.ru>
2020-04-12 21:34:14 +0300

    Домашнее задание 1. Исправленное

--- index.js
+++
@@ -1,6 +0,0 @@
-//commentary test
-alert("123");
-console.log(12222);
-var abc = "Test commit";
-var less1 = 1;
-alert(less1);
---
+++ src/index.js
@@ -0,0 +1,6 @@
+console.log('My Minimal React Webpack Babel Setup');
+
+const obj={name:'Yaroslav', age: 48, id:1};
+let {name} = obj;
+   console.log(name);
+   
